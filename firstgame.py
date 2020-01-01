19f12402321c4d260bdfc2495e02ac0c2033a331
Pihlaja Arto <arto.pihlaja@bilot.fi>
2020-01-01 12:25:01 +0200

    multiple snakes + obstacles

--- firstgame.py
+++ firstgame.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from random import randint
+from copy import deepcopy
 
 class FgRectangle():
     def __init__(self, startX, startY, width, height):        
@@ -37,6 +39,15 @@ class FgRectangle():
         else:
             return False
 
+class Step():
+    LEFT = 1
+    RIGHT = 2
+    UP = 3
+    DOWN = 4
+    def __init__(self, size, direction):
+        self.size = size
+        self.direction = direction
+
 class GameCharacter():
     def __init__(self, field, startX, startY, width, height):        
         self.field = field
@@ -45,16 +56,92 @@ class GameCharacter():
         self.y = startY
         self.width = width
         self.height = height
+        
+    def left(self):
+        return self.x
+
+    def right(self):
+        return self.x + self.width
+
+    def top(self):
+        return self.y
+
+    def bottom(self):
+        return self.y + self.height
 
     def draw(self, colour):
         pg.draw.rect(self.field.screen, colour,\
              (self.x, self.y, self.width,self.height ))
 
+    def isLeftOf(self, char2):
+        return self.right() < char2.left()
+
+    def isRightOf(self, char2):
+        return self.left() > char2.right()
+    
+    def isAbove(self, char2):
+        return self.bottom() < char2.top()
+
+    def isBelow(self, char2):
+        return self.top() > char2.bottom()
+
+    def horizontalDistFrom(self, char2):
+        # return abs(self.x - char2.x)
+        dist = 0
+        if self.isLeftOf(char2):
+            dist = char2.left() - self.right()
+        elif self.isRightOf(char2):
+            dist = self.left() - char2.right()
+        return dist
+
+    def verticalDistFrom(self, char2):
+        return abs(self.y - char2.y)
+
+
+    def stepRightCollides(self, step, char2):
+        if self.right() <= char2.left() < (self.right() + step.size)\
+        and char2.top() < self.y < char2.bottom():
+            return True
+        else:
+            return False
+
+    def stepLeftCollides(self, step, char2):        
+        if self.left() >= char2.right() > (self.left() - step.size)\
+        and char2.top() < self.y < char2.bottom():
+            return True
+        else:
+            return False
+
+    def stepUpCollides(self, step, char2):
+            if self.top() >= char2.bottom() > (self.top() - step.size)\
+            and char2.left() < self.x < char2.right():
+                return True
+            else:
+                return False
+
+    def stepDownCollides(self, step, char2):
+        if self.bottom() <= char2.top()  < (self.bottom() + step.size)\
+        and char2.left() < self.x < char2.right():
+            return True
+        else:
+            return False
+
+    def stepCollidesWith(self, step, char2):
+        if step.direction == step.RIGHT:
+            return self.stepRightCollides(step, char2)
+        elif step.direction == step.LEFT:             
+            return self.stepLeftCollides(step, char2)
+        elif step.direction == step.UP:             
+            return self.stepUpCollides(step, char2)
+        elif step.direction == step.DOWN:             
+            return self.stepDownCollides(step, char2)
+
+
 
 class FgHero(GameCharacter):
     def __init__(self, field, startX, startY, width, height):        
         super().__init__(field, startX, startY, width, height)        
-        self.step = 5 
+        self.speed = 0 
         self.prevKey = []
 
     def move(self):
@@ -63,18 +150,18 @@ class FgHero(GameCharacter):
             return
                 
         if keys == self.prevKey:
-            self.step += 2  
+            self.speed += 2  
         else:
-            self.step = 3          
+            self.speed = 3          
             
         if keys[pg.K_LEFT]:
-            self.x -= self.step
+            self.x -= self.speed
         if keys[pg.K_RIGHT]:
-            self.x += self.step
+            self.x += self.speed
         if keys[pg.K_UP]:
-            self.y -= self.step
+            self.y -= self.speed
         if keys[pg.K_DOWN]:
-            self.y += self.step
+            self.y += self.speed
         if self.x > (self.field.width - self.width):
             self.x = (self.field.width - self.width)
         if self.x < 0:
@@ -84,6 +171,9 @@ class FgHero(GameCharacter):
         if self.y < self.field.topMargin:
             self.y = self.field.topMargin
         self.prevKey = keys        
+    
+    def draw(self):
+        super().draw(self.field.GREEN)
 
 class Snake(GameCharacter):
     def __init__(self, field, startX, startY, width, height):        
@@ -92,36 +182,95 @@ class Snake(GameCharacter):
         self.speed = 3
         self.body = []
 
-    def move(self, hero):
-        self.body.insert(0, (self.x, self.y))
-        if abs(self.x - hero.x) > self.speed:
-            if self.x < hero.x:
-                self.x += self.speed
-            if self.x > hero.x:
-                self.x -= self.speed
+    def makeStep(self, step):
+        if step.direction == step.LEFT:
+            self.x -= step.size
+        elif step.direction == step.RIGHT:
+            self.x += step.size
+        elif step.direction == step.UP:
+            self.y -= step.size
+        elif step.direction == step.DOWN:
+            self.y += step.size
+
+    def move(self, characters):
+        hero = characters["hero"]
+        makeMove = True
+        heroCaught = False
+        hDist = self.horizontalDistFrom(hero)
+        vDist = self.verticalDistFrom(hero)  
+        step = Step(1, 0)
+        if self.isLeftOf(hero):
+            step.direction = step.RIGHT
+            distance = hDist
+        elif self.isRightOf(hero):
+            step.direction = step.LEFT
+            distance = hDist
+        elif self.isAbove(hero):
+            step.direction = step.DOWN
+            distance = vDist
+        elif self.isBelow(hero):
+            step.direction = step.UP 
+            distance = vDist
         else:
-            self.x = hero.x
-            if abs(self.y - hero.y) < self.speed:
-                self.y = hero.y
-            else:
-                if self.y < hero.y:
-                    self.y += self.speed            
-                if self.y > hero.y:
-                    self.y -= self.speed           
+            distance = 0
+            heroCaught = True
+
+        if distance > self.speed:
+            step.size = self.speed
+        else:
+            step.size = distance                            
+        
+        for o in characters["obstacles"]:
+                if self.stepCollidesWith(step, o):
+                    # Can't move towards, so move in other direction
+                    if step.direction == step.LEFT:
+                        step.direction = step.DOWN
+                    elif step.direction == step.RIGHT:
+                        step.direction = step.UP
+                    elif step.direction == step.UP:
+                        step.direction = step.LEFT
+                    elif step.direction == step.DOWN:
+                        step.direction = step.RIGHT                        
+                    break
+
+        self.makeStep(step)
         self.body.insert(0, (self.x, self.y)) 
+
         while len(self.body) >= self.length:
             self.body.pop()
+        
+        return heroCaught
 
     def grow(self):
-        self.length += 2
+        self.length += 1
 
     def speedUp(self):
         self.speed += 1
 
-    def draw(self, colour):
+    def draw(self):
         for p in reversed(self.body):
             self.x, self.y = p
-            super().draw(colour)
+            super().draw(self.field.RED)
+
+class Obstacle(GameCharacter):
+    def __init__(self, field, startX, startY, width, height):        
+        super().__init__(field, startX, startY, width, height)
+        self.colour = (100,100,100)        
+    
+    def hideOld(self):
+        super().draw(self.field.BLACK)
+
+    def move(self, characters):
+        r = randint(0, 2)
+        # if r <1: #swap orientation
+        #     tmpWidth = self.width
+        #     self.width = self.height        
+        #     self.height = tmpWidth        
+        self.x = randint(0, (self.field.width - self.width))
+        self.y = randint(40, (self.field.height - self.height))        
+
+    def draw(self):
+        super().draw(self.colour)
 
 class ScreenProcessor():
     BLACK = (0,0,0)
@@ -141,16 +290,12 @@ class ScreenProcessor():
     def drawCharacter(self, character, colour):
         pg.draw.rect(self.screen, colour, (character.x, character.y, character.width, character.height ))
 
-    def clearCharacters(self, hero, villain):
+    def clearCharacters(self):
         self.screen.fill(self.BLACK)
-
-    def redrawCharacters(self, hero, villain):
-        hero.draw(self.GREEN)
-        villain.draw(self.RED)
 
     def showScore(self, score):
         text = self.scoreFont.render(str(score), True, self.WHITE)
-        self.screen.blit(text, ((self.width - 30),10))
+        self.screen.blit(text, ((self.width - 40),10))
 
     def showGameOver(self):
         font = pg.font.SysFont("arial", 20, True)
@@ -161,43 +306,77 @@ class ScreenProcessor():
 class GameDriver():
     def __init__(self, sp):
         self.field = sp       
-        self.hero = FgHero(self.field, 50, 50, 6, 8)
-        self.villain = Snake(self.field, 0, 50, 10, 10)
+        self.hero = FgHero(self.field, 10, 50, 6, 8)
+        self.snakes = [Snake(self.field, 10, 10, 10, 10)] 
+        self.obstacles = [Obstacle(self.field, 100, 100, 80,10)]
         self.frame = 0
         self.score = 0
+        self.characters = {"hero": self.hero, "snakes": self.snakes, "obstacles": self.obstacles}
+   
+    def updateGame(self):   
+        heroCaught = self.moveCharacters()     
+        self.frame +=1
+        if self.frame % 50 == 0:    
+            for s in self.characters["snakes"]:  
+                s.grow()
+                s.speedUp()        
+
+        if self.frame % 200 == 0:
+            self.characters["snakes"].append(Snake(self.field, 0, 0, 10, 10))
+    
+        if self.frame % 50 == 0:
+            for o in self.characters["obstacles"]:
+                o.move(self.characters)        
+
+        if self.frame % 200 == 0:
+            self.characters["obstacles"].append(Obstacle(self.field, 0, 30, 80,10))        
+            
+        self.updateScore()
+        
+        return heroCaught
 
     def moveCharacters(self):
-        self.field.clearCharacters(self.hero, self.villain)
-        self.hero.move()
-        self.villain.move(self.hero)
-        self.field.redrawCharacters(self.hero, self.villain)
+        self.field.clearCharacters()
         self.frame +=1
-        if self.frame % 10 == 0:
-            self.score += 10
-        if self.frame % 20 == 0:
-            self.villain.grow()
-        if self.frame % 40 == 0:    
-            self.villain.speedUp()
+
+        self.characters["hero"].move()
+        self.characters["hero"].draw()
+        heroCaught = False
+        for s in self.characters["snakes"]:
+            heroCaught = s.move(self.characters)
+            s.draw()
+            if heroCaught:
+                break            
+
+        for o in self.characters["obstacles"]:
+            o.draw()
+
+        return heroCaught
 
     def updateScore(self):
+        if self.frame % 10 == 0:
+            self.score += 10
         self.field.showScore(self.score)
 
     def noCollision(self):
         noCol = True
-        for p in self.villain.body:
-            sx, sy = p
-            rectVillain = FgRectangle(sx, sy, self.villain.width, self.villain.height)
-            rectHero = FgRectangle(self.hero.x, self.hero.y, self.hero.width, self.hero.height)
-            if rectVillain.overLapWithRectangle(rectHero):
-                noCol = False
+        hero = self.characters["hero"]
+        for s in self.characters["snakes"]:
+            for p in s.body:
+                sx, sy = p
+                rectSnake = FgRectangle(sx, sy, s.width, s.height)
+                rectHero = FgRectangle(hero.x, hero.y, hero.width, hero.height)
+                if rectSnake.overLapWithRectangle(rectHero):
+                    noCol = False
+                    break
+                del rectHero
+                del rectSnake
+            if noCol == False:
                 break
-            del rectHero
-            del rectVillain
         return noCol
 
     def playAgain(self):        
         self.field.showGameOver()
-        keys = []
         validResponse = False
         while not(validResponse):
             for event in pg.event.get():
@@ -218,16 +397,18 @@ def main():
     while playMore:
         gd = GameDriver(sp)
         gameOn = True
+        heroCaught = False
         while gameOn:
+            heroCaught = gd.updateGame()
+            # gameOn = gd.noCollision()
+            clock.tick(10)
+            pg.display.update()
+            if heroCaught:
+                gameOn = False
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     gameOn = False
                     playMore = False                        
-            gd.moveCharacters()
-            gameOn = gd.noCollision()
-            gd.updateScore()
-            clock.tick(10)
-            pg.display.update()
         playMore = gd.playAgain()    
     pg.quit()
 
--- test_first_game.py
+++ test_first_game.py
@@ -13,6 +13,29 @@ class MockFgField():
         self.height = 500
         self.screen = []
 
+class TestGameCharacter(unittest.TestCase):
+    def setUp(self):
+        self.obstacle = fg.GameCharacter(mock.Mock(), 100, 100, 10, 80)
+        self.movingCharacter = fg.GameCharacter(mock.Mock(), 0, 0, 10, 10)
+
+    def test_stepRightCollides(self): 
+        self.movingCharacter.x = 89
+        self.movingCharacter.y = 120
+        step = fg.Step(20, fg.Step.RIGHT)
+        self.assertEqual(self.movingCharacter.stepRightCollides(step, self.obstacle), True,\
+             "Failed to notice that step right crosses obstacle")
+
+    def test_stepLeftCollides(self): 
+        self.movingCharacter.x = 112
+        self.movingCharacter.y = 120
+        step = fg.Step(4, fg.Step.LEFT)
+        self.assertEqual(self.movingCharacter.stepLeftCollides(step, self.obstacle), True,\
+             "Failed to notice that step left crosses obstacle")
+
+        self.movingCharacter.y = 80
+        self.assertEqual(self.movingCharacter.stepLeftCollides(step, self.obstacle), False,\
+             "Failed to notice that step left goes over obstacle")        
+
 class TestCollisionDetection(unittest.TestCase):
     def setUp(self):
         self.rect1 = fg.FgRectangle(100, 100, 10, 10)
@@ -66,14 +89,21 @@ class MockEvent():
 class TestGameDriver(unittest.TestCase):
     def setUp(self):
         self.gd = fg.GameDriver(mock.Mock())
+        self.gd.field.width = 300
+        self.gd.field.height = 300
+        self.gd.hero.x = 10
+        self.gd.hero.y = 10
+        
 
     @mock.patch.object(fg.FgHero, "move")
-    def test_movecharacters(self, mock_move):
+    @mock.patch.object(fg.GameCharacter, "draw")
+    def test_movecharacters(self, mock_move, mock_draw):
         for i in range(0,40):
             self.gd.moveCharacters()
         self.assertEqual(self.gd.score,40,"Failed to count score")
-        self.assertEqual(self.gd.villain.speed, 4, "Failed speeding up snake")
-        self.assertEqual(self.gd.villain.length,7, "Failed growing snake")
+        for s in self.gd.characters["snakes"]:
+            self.assertEqual(s.speed, 4, "Failed speeding up snake")
+            self.assertEqual(s.length,5, "Failed growing snake")
 
     @mock.patch.object(pg.event, "get")
     def test_playAgain(self, mock_get):
